faca8d7f1959b239c38b900aab459fb9ffd15b66
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-21 18:25:13 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Person
   
   attr_accessor :name, :birthday, :hair_color, :eye_color, 
-    :weight, :handed, :complexion, :t_shirt_size, 
+    :weight, :handed, :complexion, :t_shirt_size, :height,
     :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(avi_attibutes)
