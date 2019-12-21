a44f77779a2cd46b0c72d8a6e924dacf51e5db1d
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-21 18:11:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,22 +5,8 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :plant_length, :pant_width
   
   def initialize(avi_attibutes)
-    avi_attibutes = {:name => "Avi",
-      :birthday => "01/29/1984",
-      :hair_color => "brown",
-      :eye_color => "brown",
-      :height => "short",
-      :weight => "good",
-      :handed => "lefty",
-      :complexion => "decent",
-      :t_shirt_size => "medium",
-      :wrist_size => "small",
-      :glove_size => "normal",
-      :pant_length => "32",
-      :pant_width => "32"}
-    end
-   
-   def avi_attributes
-     avi_attibutes.map{|i| i.name == "Avi"}
-   end
+    avi_attibutes.each{|key, value|}
+    binding.pry
+    
+  end
 end
