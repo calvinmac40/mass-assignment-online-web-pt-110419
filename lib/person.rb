aee015765dd3c0097102d3245625b46d585a5086
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-21 18:18:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,6 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :plant_length, :pant_width
   
   def initialize(avi_attibutes)
-    avi_attibutes.each{|key, value| }
+    avi_attibutes.each{|key, value| self.send}
   end
 end
